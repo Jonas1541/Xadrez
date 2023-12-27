fa370d4883b0a7c8722ecc1ec9ae697936184e13
Jonas <jonasdurau@gmail.com>
2023-12-27 19:55:07 -0300

    First class: Position

--- app/src/main/java/xadrez/App.java
+++ app/src/main/java/xadrez/App.java
@@ -3,12 +3,11 @@
  */
 package xadrez;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import xadrez.boardgame.Position;
 
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
