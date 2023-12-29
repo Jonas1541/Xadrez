aa5876c2021f2bd7d704febd8d17df4a140fbca3
Jonas <jonasdurau@gmail.com>
2023-12-29 20:10:44 -0300

    Starting to implement Board and Piece

--- app/src/main/java/xadrez/App.java
+++ app/src/main/java/xadrez/App.java
@@ -3,11 +3,10 @@
  */
 package xadrez;
 
-import xadrez.boardgame.Position;
+import xadrez.boardgame.Board;
 
 public class App {
     public static void main(String[] args) {
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
